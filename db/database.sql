ddbc5c75e88196f0104687a29b00168352d46d29
TheoBiron <yami-theo@hotmail.fr>
2015-10-08 17:05:16 +0200

    v.0.1

---
+++ db/database.sql
@@ -0,0 +1,4 @@
+create database if not exists ananasanam character set utf8 collate utf8_unicode_ci;
+use ananasanam;
+
+grant all privileges on ananasanam.* to 'admin'@'localhost' identified by 'suigetsu1995';
---
+++ db/structure.sql
@@ -0,0 +1,16 @@
+drop table if exists t_user;
+drop table if exists t_article;
+
+create table t_article (
+    art_id integer not null primary key auto_increment,
+    art_title varchar(100) not null,
+    art_content varchar(2000) not null
+) engine=innodb character set utf8 collate utf8_unicode_ci;
+
+create table t_user (
+    usr_id integer not null primary key auto_increment,
+    usr_name varchar(50) not null,
+    usr_password varchar(88) not null,
+    usr_salt varchar(23) not null,
+    usr_role varchar(50) not null 
+) engine=innodb character set utf8 collate utf8_unicode_ci;
